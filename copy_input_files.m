1274c2d860acf61f1c3737d98e63966bfdefce8d
Tobias Leutritz <tleutritz@cbs.mpg.de>
2020-03-13 19:12:39 +0100

    minor changes

--- copy_input_files.m
+++ copy_input_files.m
@@ -1,40 +1,42 @@
 function inputs = copy_input_files(NIFTI_path,old_inputs,sub_folder)
 inputs = old_inputs;
 for inp_ind = 2:8
-    for fil_ind = 1:numel(old_inputs{inp_ind,1})
-        [cur_path,cur_file,ext] = fileparts(old_inputs{inp_ind,1}{fil_ind});
-        fs_ind = strfind(cur_path,filesep); % indices of filesep to find folders
-        ar = fullfile(cur_path(1:fs_ind(end)),sub_folder);
-        if ~exist(ar,'dir')
-            mkdir(ar);
-        end
-        
-        % restructure inputs with new paths and copy files
-        new_path = fullfile(ar,cur_path(fs_ind(end):end));
-        new_file = fullfile(new_path,strcat(cur_file,ext));
-        old_file = fullfile(cur_path,strcat(cur_file,ext));
-        if (inp_ind == 7) && (fil_ind == 1) % replace 1st PDw filename with PatientID as basename of MPMs
-            patID = get_metadata_val(old_file,'PatientID');
-            if iscell(patID)
-                patID = patID{1};
+    if ~isempty(old_inputs{inp_ind,1}) % check for empty input
+        for fil_ind = 1:numel(old_inputs{inp_ind,1})
+            [cur_path,cur_file,ext] = fileparts(old_inputs{inp_ind,1}{fil_ind});
+            fs_ind = strfind(cur_path,filesep); % indices of filesep to find folders
+            ar = fullfile(cur_path(1:fs_ind(end)),sub_folder);
+            if ~exist(ar,'dir')
+                mkdir(ar);
             end
-            patID = deblank(patID);
-            new_file = strrep(new_file,spm_file(new_file,'basename'),patID); 
-        end
-        if contains(new_file,',1')
-            new_file = strrep(new_file,',1','');
-            old_file = strrep(old_file,',1','');
-        end
-        inputs{inp_ind,1}{fil_ind} = new_file;
-        if ~exist(new_file,'file')
-            if ~exist(new_path,'dir')
-                mkdir(new_path);
+            
+            % restructure inputs with new paths and copy files
+            new_path = fullfile(ar,cur_path(fs_ind(end):end));
+            new_file = fullfile(new_path,strcat(cur_file,ext));
+            old_file = fullfile(cur_path,strcat(cur_file,ext));
+            if (inp_ind == 7) && (fil_ind == 1) % replace 1st PDw filename with PatientID as basename of MPMs
+                patID = get_metadata_val(old_file,'PatientID');
+                if iscell(patID)
+                    patID = patID{1};
+                end
+                patID = deblank(patID);
+                new_file = strrep(new_file,spm_file(new_file,'basename'),patID);
             end
-            copyfile(old_file,new_file); % explicitly specify destination folder,
-            % otherwise just contents will end up there and not the folder structure itself
-        end
-        if ~exist(fullfile(new_path,strcat(cur_file,'.json')),'file')
-            copyfile(strrep(old_file,'.nii','.json'),strrep(new_file,'.nii','.json'));
+            if contains(new_file,',1')
+                new_file = strrep(new_file,',1','');
+                old_file = strrep(old_file,',1','');
+            end
+            inputs{inp_ind,1}{fil_ind} = new_file;
+            if ~exist(new_file,'file')
+                if ~exist(new_path,'dir')
+                    mkdir(new_path);
+                end
+                copyfile(old_file,new_file); % explicitly specify destination folder,
+                % otherwise just contents will end up there and not the folder structure itself
+            end
+            if ~exist(fullfile(new_path,strcat(cur_file,'.json')),'file')
+                copyfile(strrep(old_file,'.nii','.json'),strrep(new_file,'.nii','.json'));
+            end
         end
     end
 end
