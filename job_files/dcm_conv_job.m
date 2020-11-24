92bc3df5c6de6d27a29b13491e9955bec49f6369
Tobias Leutritz <tleutritz@cbs.mpg.de>
2020-11-24 13:18:53 +0100

    extend DICOM conversion to include patientID and scan date

--- job_files/dcm_conv_job.m
+++ job_files/dcm_conv_job.m
@@ -1,10 +1,10 @@
 %-----------------------------------------------------------------------
-% Job saved on 22-Feb-2019 15:12:43 by cfg_util (rev $Rev: 6460 $)
+% Job saved on 24-Nov-2020 11:18:02 by cfg_util (rev $Rev: 6460 $)
 % spm SPM - SPM12 (6906)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.tools.hmri.dicom.data = '<UNDEFINED>';
-matlabbatch{1}.spm.tools.hmri.dicom.root = 'series';
+matlabbatch{1}.spm.tools.hmri.dicom.root = 'patid_date';
 matlabbatch{1}.spm.tools.hmri.dicom.outdir = '<UNDEFINED>';
 matlabbatch{1}.spm.tools.hmri.dicom.protfilter = '.*';
 matlabbatch{1}.spm.tools.hmri.dicom.convopts.format = 'nii';
